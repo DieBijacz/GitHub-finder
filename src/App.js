e4c45e39c8f1fd0587770b17a209b3a88f2202d5
masta <mastanilewicz@gmail.com>
2021-12-30 13:57:19 +0000

    fetching users and display

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
+import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import { GithubProvider } from './context/github/GithubContext';
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-          <main>
-            Content
-          </main>
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router> 
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../components/users/UserResults"
+
+function Home() {
+  return (
+    <>
+      {/* search component here */}
+      <UserResults />
+    </>
+
+  )
+}
+
+export default Home
